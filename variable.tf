74257d584489d6953bdebcc3614321efd9537d33
ArfaBeast <149589407+ArfaBeast@users.noreply.github.com>
2024-01-09 01:13:45 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "RG-location" {
+  type = string
+}
+variable "env" {
+  type = string
+}
+variable "app" {
+  type = string
+}
+variable "secret-id" {
+  type = string
+}
+variable "subscription_id" {
+  type = string
+}
